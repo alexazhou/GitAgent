5118104f2d85291caf1f51d4bcaf0a2cbd2367dc
AlexaZhou <AlexaZhou@163.com>
2016-07-24 23:45:04 +0800

    修正协议

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: LGPL License',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
