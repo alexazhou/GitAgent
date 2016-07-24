0b271c6252a0452cb300a407a1415f8dd624b16f
AlexaZhou <AlexaZhou@163.com>
2016-07-24 23:58:36 +0800

    加入 client 模块

---
+++ gitagent/client.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date    : 2016-07-24 
+# @Author  : Alexa (AlexaZhou@163.com)
+# @Link    : 
+# @Disc    : 
+
+import requests
+
--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['tornado','GitPython'],
+    install_requires=['tornado','GitPython','requests'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
